d8e18e0003ad89064c71324ada05750ae98acce3
Max Sabino <sabinobkp2022@gmail.com>
2025-09-25 10:12:56 -0300

    Alt deploy Git-Netlify build2

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -4,7 +4,8 @@ import axios from "axios";
 // const { NODE_ENV } = process.env;
 // const URL = "http://localhost:3001/api";
 // const URL = "https://deploy-jornada-full-stack.onrender.com/api";
-const URL = "https://repricaspotify.netlify.app/api";
+// const URL = "https://repricaspotify.netlify.app/api";
+const URL = "https://maxsabino.github.io/RepricaSpotify/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
