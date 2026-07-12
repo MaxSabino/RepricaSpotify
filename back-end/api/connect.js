ba6236cd515b54adc9dd14437b33e4088ad28726
sabino <161085650+MaxSabino@users.noreply.github.com>
2026-07-12 13:40:23 -0300

    Update MongoDB connection URI for RepSpotify

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -4,7 +4,9 @@
 import { MongoClient } from "mongodb";
 
 const URI =
-  "mongodb+srv://fullstackjornada:qojI71xVU2aV8UKC@cluster0.v1qra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://repspotify:I1vwJzJLgJdQ3ItS@cluster0.d3qkh.mongodb.net/RepSpotify?appName=Cluster0";
+
+//mongodb+srv://fullstackjornada:qojI71xVU2aV8UKC@cluster0.v1qra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";(original)
 
 const client = new MongoClient(URI);
 
